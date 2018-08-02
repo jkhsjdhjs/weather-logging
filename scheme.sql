7bd4d3d445daef0e5ee98631065a63325a26aab1
Nils Schweinsberg <mail@nils.cc>
2018-08-02 22:37:36 +0200

    Update schema with hourly/daily statistics

--- scheme.sql
+++ scheme.sql
@@ -1,4 +1,8 @@
-CREATE TABLE weather (
+CREATE SCHEMA IF NOT EXISTS weather_logging;
+
+SET search_path = weather_logging;
+
+CREATE TABLE IF NOT EXISTS weather (
     id SERIAL PRIMARY KEY,
     ds1820_temp NUMERIC(6,3),
     am2302_temp NUMERIC(4,1),
@@ -8,4 +12,38 @@ CREATE TABLE weather (
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX ON weather (created_at);
+CREATE INDEX IF NOT EXISTS weather_created_at ON weather (created_at);
+
+CREATE OR REPLACE VIEW weather_daily AS
+  SELECT
+    date_trunc('day', created_at) as day,
+    avg(ds1820_temp) as temp_avg,
+    stddev_pop(ds1820_temp) as temp_dev,
+    avg(am2302_humidity) as humidity_avg,
+    stddev_pop(am2302_humidity) as humidity_dev,
+    avg(bmp180_pressure) as pressure_avg,
+    stddev_pop(bmp180_pressure) as pressure_dev
+  FROM
+    weather
+  GROUP BY
+    date_trunc('day', created_at)
+  ORDER BY
+    date_trunc('day', created_at)
+;
+
+CREATE OR REPLACE VIEW weather_hourly AS
+  SELECT
+    date_trunc('hour', created_at) as hour,
+    avg(ds1820_temp) as temp_avg,
+    stddev_pop(ds1820_temp) as temp_dev,
+    avg(am2302_humidity) as humidity_avg,
+    stddev_pop(am2302_humidity) as humidity_dev,
+    avg(bmp180_pressure) as pressure_avg,
+    stddev_pop(bmp180_pressure) as pressure_dev
+  FROM
+    weather
+  GROUP BY
+    date_trunc('hour', created_at)
+  ORDER BY
+    date_trunc('hour', created_at)
+;
