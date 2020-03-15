ff6c17d7b7c894d7114d0afe8b18c4d87bdbae39
jkhsjdhjs <leon.moeller@yahoo.de>
2020-03-15 23:27:27 +0100

    rewrite most of the script add more sanity checks

--- log-weather.php
+++ log-weather.php
@@ -1,6 +1,37 @@
+#!/usr/bin/env php
 <?php
 
-const API_KEY = "";
+class SensorData {
+    function __construct($temperature = null, $humidity = null, $pressure = null) {
+        $this->temperature = $temperature;
+        $this->humidity = $humidity;
+        $this->pressure = $pressure;
+    }
+}
+
+class Sensor {
+    public $data = null;
+    function __construct($name, $read_fnc) {
+        $this->name = $name;
+        $this->read_fnc = $read_fnc;
+    }
+    function read() {
+        $data = call_user_func($this->read_fnc);
+        if($data === false)
+            return false;
+        return $this->data = $data;
+    }
+    function check_sanity() {
+        $sanity = [];
+        if($this->data->temperature)
+            $sanity[] = check_temperature($this->data->temperature);
+        if($this->data->humidity)
+            $sanity[] = check_humidity($this->data->humidity);
+        if($this->data->pressure)
+            $sanity[] = check_pressure($this->data->pressure);
+        return !in_array(false, $sanity);
+    }
+}
 
 // function for http requests
 function fetch($url, $method = "GET", $data = null, $content_type = "application/x-www-form-urlencoded") {
@@ -26,6 +57,27 @@ function fetch($url, $method = "GET", $data = null, $content_type = "application
     return file_get_contents($url, false, stream_context_create(["http" => $context]));
 }
 
+// sensor reading functions
+function read_ds1820() {
+    $ds1820_raw = file_get_contents("/sys/bus/w1/devices/10-000802e481ae/w1_slave");
+    if($ds1820_raw !== false && preg_match("/crc=.{3}YES.*t=(\d+)$/s", $ds1820_raw, $matches))
+        return new SensorData($matches[1] / 1000);
+    return false;
+}
+
+function read_am2302() {
+    if(preg_match("/(\d{2}\.\d{2}).+(\d{2}\.\d{2})/", `lol_dht22/loldht 0 10 0`, $matches))
+        return new SensorData(floatval($matches[2]), floatval($matches[1]));
+    return false;
+}
+
+function read_bmp180() {
+    $bmp180_data = json_decode(`BMP180/read.py`);
+    if(!is_null($bmp180_data))
+        return new SensorData($bmp180_data->temp, null, $bmp180_data->pressure);
+    return false;
+}
+
 // sanity checks
 function check_temperature($temp) {
     return $temp > -20 && $temp < 60;
@@ -39,67 +91,96 @@ function check_pressure($pressure) {
     return $pressure > 800 && $pressure < 1200;
 }
 
-function sensor_sanity_check($sane, $sensor) {
-    static $tries = 0;
-    $delay = 5;
-    if(!$sane && $tries < 2) {
-        $tries++;
-        fwrite(STDERR, "Sanity check failed for $sensor!" . PHP_EOL);
-        fwrite(STDERR, "Retrying in $delay seconds..." . PHP_EOL);
+function read_sensor($sensor, $tries, $delay) {
+    for($i = 0; true; $i++) {
+        if($sensor->read() !== false)
+            if($sensor->check_sanity())
+                return true;
+            else
+                fwrite(STDERR, "sanity check");
+        else
+            fwrite(STDERR, "reading");
+        fwrite(STDERR, " failed for " . $sensor->name . PHP_EOL . "data: " . json_encode($sensor->data) . PHP_EOL);
+        if($i >= $tries - 1) {
+            fwrite(STDERR, "$tries readings failed for $sensor->name! ");
+            if($sensor->data !== null) {
+                fwrite(STDERR, "using last data that failed the sanity check." . PHP_EOL);
+                return true;
+            }
+            else {
+                fwrite(STDERR, "aborting." . PHP_EOL);
+                return false;
+            }
+        }
+        fwrite(STDERR, "retrying in $delay seconds..." . PHP_EOL);
         sleep($delay);
-        return false;
     }
-    if($tries >= 2)
-        fwrite(STDERR, "Sanity check failed for $sensor for the third time! Ignoring..." . PHP_EOL);
-    $tries = 0;
-    return true;
 }
 
-// read sensors
-$ds1820 = (object) [
-    "temp" => null
-];
-$am2302 = (object) [
-    "temp" => null,
-    "humidity" => null
-];
-$bmp180 = (object) [
-    "temp" => null,
-    "pressure" => null
+
+/* --------------------- */
+/* --- program start --- */
+/* --------------------- */
+
+const API_KEY = "";
+const SENSOR_READ_DELAY = 5;
+const SENSOR_READ_TRIES = 3;
+const DEVIATION_CHECK_TRIES = 2;
+const HIGH_DEVIATION = 5;
+const COMPLETE_REREAD_TRIES = 1;
+$SENSORS = [
+    new Sensor("DS1820", 'read_ds1820'),
+    new Sensor("AM2302", 'read_am2302'),
+    new Sensor("BMP180", 'read_bmp180')
 ];
 
-do {
-    $ds1820_raw = file_get_contents("/sys/bus/w1/devices/10-000802e481ae/w1_slave");
-    if($ds1820_raw !== false && preg_match("/crc=.{3}YES.*t=(\d+)$/s", $ds1820_raw, $matches))
-        $ds1820->temp = $matches[1] / 1000;
-    else
-        fwrite(STDERR, "Couldn't read DS1820!" . PHP_EOL);
-} while(!sensor_sanity_check(check_temperature($ds1820->temp), "DS1820"));
-
-do {
-    if(preg_match("/(\d{2}\.\d{2}).+(\d{2}\.\d{2})/", `lol_dht22/loldht 0 10 0`, $matches)) {
-        $am2302->humidity = floatval($matches[1]);
-        $am2302->temp = floatval($matches[2]);
+// read sensors and do simple sanity checks
+$reread = true;
+for($retry = 0; $retry <= COMPLETE_REREAD_TRIES && $reread; $retry++) {
+    $reread = false;
+    foreach($SENSORS as $sensor) {
+        if(!read_sensor($sensor, SENSOR_READ_TRIES, SENSOR_READ_DELAY))
+            exit(1);
     }
-    else
-        fwrite(STDERR, "Couldn't read AM2302!\n");
-} while(!sensor_sanity_check(check_temperature($am2302->temp) && check_humidity($am2302->humidity), "AM2302"));
 
-do {
-    $bmp180_json = json_decode(`BMP180/read.py`);
-    if(!is_null($bmp180_json))
-        $bmp180 = $bmp180_json;
-    else
-        fwrite(STDERR, "Couldn't read BMP180!\n");
-} while(!sensor_sanity_check(check_temperature($bmp180->temp) && check_pressure($bmp180->pressure), "BMP180"));
+    // temperature sanity check by checking the deviation from median
+    $repeat = true;
+    for($i = 0; $i <= DEVIATION_CHECK_TRIES && $repeat; $i++) {
+        $repeat = false;
+        $temperatures = array_map(function($s) { return $s->data->temperature; }, $SENSORS);
+        sort($temperatures);
+        $median = $temperatures[ceil(count($temperatures) / 2 - 1)];
+        foreach($SENSORS as $sensor) {
+            // if deviation is too high, re-read
+            if(abs($sensor->data->temperature - $median) > HIGH_DEVIATION) {
+                fwrite(STDERR, "$sensor->name has a high deviation from median, ");
+                if($i >= DEVIATION_CHECK_TRIES) {
+                    if($retry >= COMPLETE_REREAD_TRIES)
+                        fwrite(STDERR, "ignoring..." . PHP_EOL);
+                    else
+                        fwrite(STDERR, "re-reading all sensors..." . PHP_EOL);
+                    $reread = true;
+                    break;
+                }
+                fwrite(STDERR, "re-reading in " . SENSOR_READ_DELAY . " seconds..." . PHP_EOL);
+                sleep(SENSOR_READ_DELAY);
+                // it's impossible for this to return false since it already has data cached
+                read_sensor($sensor, SENSOR_READ_TRIES, SENSOR_READ_DELAY);
+                $repeat = true;
+                break;
+            }
+        }
+    }
+}
 
+[$ds1820, $am2302, $bmp180] = array_map(function($s) { return $s->data; }, $SENSORS);
 
 $response = fetch("https://weather.totally.rip/api/v2/insert.php", "POST", [
     "secret" => API_KEY,
-    "ds1820_temp" => $ds1820->temp,
-    "am2302_temp" => $am2302->temp,
+    "ds1820_temp" => $ds1820->temperature,
+    "am2302_temp" => $am2302->temperature,
     "am2302_humidity" => $am2302->humidity,
-    "bmp180_temp" => $bmp180->temp,
+    "bmp180_temp" => $bmp180->temperature,
     "bmp180_pressure" => $bmp180->pressure
 ]);
 
